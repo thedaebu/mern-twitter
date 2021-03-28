d1dcac99a59e07d73ba6446bfe78b7794e512503
thedaebu <min.kim163@gmail.com>
2021-03-28 13:45:05 -0400

    fix jwterror try 1

--- config/passport.js
+++ config/passport.js
@@ -2,14 +2,14 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-const keys = require('../config/keys');
+const keys = require('./keys');
 
-const options = {};
-options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = keys.secretOrKey;
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-  passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
     User.findById(jwt_payload.id)
       .then(user => {
         if (user) {
